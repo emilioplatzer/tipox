af1d7daddce2bf7891b9a6ae337f75a388bebd7e
emilioplatzer@gmail.com <emilioplatzer@gmail.com@25d175b5-64de-e66e-94b9-818158d59e08>
2013-03-09 22:02:40 +0000

    Primer página de ejemplo sin nada.

---
+++ trunk/fuentes/alserver/ejemplo_info/main.js
@@ -0,0 +1,9 @@
+﻿// Por $Author Revisión $Revision del $Date
+
+Aplicacion.prototype.mostrarPaginaActual=function(){
+    document.body.innerHTML='';
+    this.grab(document.body,{tipox:'h1', innerText:'Primer página de Ejemplo'});
+    this.grab(document.body,"próximamente");
+}
+
+Aplicacion.run(new Aplicacion());
---
+++ trunk/fuentes/alserver/tipox/app.js
@@ -0,0 +1,48 @@
+﻿"use strict";
+
+function Aplicacion(){
+}
+
+Aplicacion.prototype.domCreator=function(tipox){
+    return {
+        nuevo:function(tagName){ return document.createElement(tagName); },
+        asignarAtributos:function(destino,definicion){
+            for(var atributo in definicion) if(definicion.hasOwnProperty(atributo)){
+                var valor=definicion[atributo];
+                if(atributo!='tipox'){
+                    if(atributo instanceof Object){
+                        this.asignarAtributos(destino[atributo],valor);
+                    }else{
+                        destino[atributo]=valor;
+                    }
+                }
+            }
+        }
+    }
+}
+
+Aplicacion.prototype.grab=function(elemento,definicion){
+    var nuevoElemento;
+    if(typeof(definicion)=='string'){
+        nuevoElemento=document.createTextNode(definicion);
+    }else{
+        var tipox=definicion.tipox;
+        var creador=this.domCreator(tipox);
+        nuevoElemento=creador.nuevo(tipox);
+        creador.asignarAtributos(nuevoElemento,definicion);
+    }
+    elemento.appendChild(nuevoElemento);
+}
+
+Aplicacion.prototype.mostrarPaginaActual=function(){
+    this.grab(document.body,{tipox:'h1', innerText:'¡en preparación!'});
+}
+
+Aplicacion.prototype.controlarParametros=function(){}
+
+Aplicacion.run=function(app){
+    app.controlarParametros({app:app},{app:{validar:function(app){ return app instanceof Aplicacion; }}});
+    window.addEventListener('load',function(){
+        app.mostrarPaginaActual();
+    });    
+}
