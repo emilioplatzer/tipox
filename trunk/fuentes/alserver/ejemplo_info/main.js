cab4af728611a0cb451eb0ea89f8c88df39e7706
emilioplatzer@gmail.com <emilioplatzer@gmail.com@25d175b5-64de-e66e-94b9-818158d59e08>
2013-03-09 22:37:36 +0000

    Hace grab con arrays.

--- trunk/fuentes/alserver/ejemplo_info/main.js
+++ trunk/fuentes/alserver/ejemplo_info/main.js
@@ -1,9 +1,13 @@
 ﻿// Por $Author Revisión $Revision del $Date
 
-Aplicacion.prototype.mostrarPaginaActual=function(){
-    document.body.innerHTML='';
-    this.grab(document.body,{tipox:'h1', innerText:'Primer página de Ejemplo'});
-    this.grab(document.body,"próximamente");
+Aplicacion.prototype.paginas={
+    'default':[
+        {tipox:'h1', innerText:'Primer página de Ejemplo'},
+        {tipox:'tipox_logo'},
+        " quiere ser un framework para experimentar código hecho en su mayoría con JavaScript. "+
+            "La idea es experimentar también con otras ideas como mantener el estado en la barra de direcciones.",
+        {tipox:'footer', nodes:[ 'se puede ver un ejemplo de primer vínculo ', {tipox:'app.vinculo', innerText:'acá'}]},
+    ]
 }
 
 Aplicacion.run(new Aplicacion());
