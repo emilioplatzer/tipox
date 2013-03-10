df67feb38820de374dd92bba8ad8d90b8b29c579
emilioplatzer@gmail.com <emilioplatzer@gmail.com@25d175b5-64de-e66e-94b9-818158d59e08>
2013-03-10 18:17:29 +0000

    Carga js vía AJAX usando Futuros.

---
+++ trunk/fuentes/alserver/almacen/almacen.js
@@ -0,0 +1,6 @@
+﻿// Por $Author$ Revisión $Revision$ del $Date$
+"use strict";
+
+Aplicacion.prototype.almacen_instalado="sí lo está";
+
+alert('Ingresado lo nuevo: '+Aplicacion.prototype.almacen_instalado);
