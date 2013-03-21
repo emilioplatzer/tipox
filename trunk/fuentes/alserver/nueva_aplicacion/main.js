3104c5edc2b6a756505f989e08e6b67db442dd1d
emilioplatzer@gmail.com <emilioplatzer@gmail.com@25d175b5-64de-e66e-94b9-818158d59e08>
2013-03-21 00:12:07 +0000

    Renombro menu por nueva_aplicacion para que se entienda que es el punto de partida para una aplicación genérica vacía.

---
+++ trunk/fuentes/alserver/nueva_aplicacion/main.js
@@ -0,0 +1,16 @@
+﻿// Por $Author: emilioplatzer@gmail.com $ Revisión $Revision: 23 $ del $Date: 2013-03-18 01:49:46 -0300 (lun 18 de mar de 2013) $
+"use strict";
+
+Aplicacion.prototype.paginas.intr={ 
+        labelMenu:'Introducción',
+        nodes:["Para hacer una nueva aplicación",
+            "Las instrucciones principales son: ",
+            {tipox: 'lista', tagList:'ol', tagElement:'li', elementos:[
+              "Copie el contenido de la carpeta nueva_aplicacion en una nueva carpeta que esté al mismo nivel (respecto de la raíz)",
+              "Cambie la introducción y agregue o quite páginas y funciones acá", 
+              ]}
+        ]
+    };
+
+var app=new Aplicacion();
+Aplicacion.run(app);
