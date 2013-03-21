fefffc3266d1bd9fc50a5f0db7e0b91c83c2f525
emilioplatzer@gmail.com <emilioplatzer@gmail.com@25d175b5-64de-e66e-94b9-818158d59e08>
2013-03-21 00:08:25 +0000

    Oculto el primer caso. Pongo un app.php genérica y un launch.php genérico en menu para poder inicar facilmente una nueva aplicación.

---
+++ fuentes/alserver/almacen/almacen_pr.js
@@ -0,0 +1,6 @@
+﻿Aplicacion.prototype.casosDePrueba.push({
+    tipox:'estoMismo',
+    caso:'el primer campo de la pantalla entrar es el usuario (se necesita para ponerle la aclaración)',
+    entrada:[Aplicacion.prototype.paginas.entrar.nodes[1].nodes[0].id],
+    salida:'usuario'
+});
