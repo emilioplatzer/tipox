6ba322fd4b2dada8e70e9aae88e0d16711ea8ecd
emilioplatzer@gmail.com <emilioplatzer@gmail.com@25d175b5-64de-e66e-94b9-818158d59e08>
2013-10-14 18:43:11 +0000

    Agrego la función para pasar de números a letras (incluye unidad de medida pero no centavos.

---
+++ trunk/fuentes/alserver/demos/demo_letras.php
@@ -0,0 +1,11 @@
+<?php
+include "demo_probador.php";
+
+$lanzador->params['title']='Demo para poner importe en letras';
+$lanzador->params['css'][]='../tipox/tipox.css';
+$lanzador->params['js'][]='../tipox/colocadorProbador.js';
+$lanzador->params['js'][]='../tipox/letras.js';
+$lanzador->params['js'][]='demo_probador_con_colocador.js';
+
+$lanzador->lanzar();
+?>
