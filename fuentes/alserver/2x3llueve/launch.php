dd234098213eb22ffabe6e09a487e04fbdd929af
emilioplatzer@gmail.com <emilioplatzer@gmail.com@25d175b5-64de-e66e-94b9-818158d59e08>
2013-10-27 15:53:15 +0000

    Demo con dispositivo.

--- fuentes/alserver/2x3llueve/launch.php
+++ fuentes/alserver/2x3llueve/launch.php
@@ -1,16 +1,18 @@
 ﻿<?php
 include "../tipox/launch.php";
 
-lanzar(array(
+$lanzador=new Lanzador(array(
     'title'=>'2×3 llueve',
-    'jss'=>array(
-        "../tipox/tercera/md5_paj",
-        "../tipox/app_pr",
-        "2x3llueve",
-        "estruc",
-        "main"
+    'js'=>array(
+        "../tipox/tercera/md5_paj.js",
+        "2x3llueve.js"
     ),
     'css'=>array(
         "2x3llueve.css"
-    )
+    ),
+    'empezar'=>'cargando'
 ));
+
+$lanzador->lanzar();
+
+?>
---
+++ fuentes/alserver/demos/demo_dispo.js
@@ -0,0 +1,79 @@
+﻿// Por $Author$ Revisión $Revision: 23 $ del $Date$
+"use strict";
+var colocador;
+
+var NUM={claseElemento:'revisar_num'};
+var TXT={claseElemento:'revisar_txt'};
+
+var revisar={
+    outerWidth:NUM, 
+    outerHeight:NUM, 
+    innerWidth:NUM, 
+    innerHeight:NUM, 
+    navigator:{
+        userAgent:TXT
+    },
+    screen:{
+        availTop:NUM,
+        availLeft:NUM,
+        availWidth:NUM,
+        availHeight:NUM,
+        width:NUM,
+        height:NUM,
+        pixelDeph:NUM
+    },
+    document:{
+        width:NUM,
+        height:NUM,
+        body:{
+            clientWidth:NUM,
+            clientHeight:NUM,
+            offsetWidth:NUM,
+            offsetHeight:NUM,
+            scrollWidth:NUM,
+            scrollHeight:NUM
+        }
+    }
+};
+
+function mostrarRevisar(revisar, destino, objetivo){
+    var tabla=colocador.colocar({destino:destino, contenido:{tipox:'table', className:'resultados'}});
+    for(var campo in revisar){
+        var def=revisar[campo];
+        var fila=colocador.colocar({destino:tabla, contenido:{tipox:'tr', nodes:[
+            {tipox:'td', className:'label', nodes:campo}, 
+            {tipox:'td', className:'label'}
+        ]}});
+        var celda=fila.cells[fila.cells.length-1];
+        if(def.claseElemento){
+            celda.className=def.claseElemento;
+            celda.innerText=objetivo[campo];
+        }else{
+            mostrarRevisar(def, celda, objetivo[campo]);
+        }
+    }
+}
+
+window.addEventListener('load',function(){
+    colocador=new Colocador();
+    var mostrar=function(){
+        var destino=document.getElementById('muestra');
+        destino.innerHTML='';
+        mostrarRevisar(revisar,destino, window);
+    };
+    colocador.colocar({
+        destino:document.body,
+        contenido:[
+            {tipox:'div', nodes:[
+                {tipox:'h1', id:'debug_html', nodes:["Demo dispo ", {tipox:'small', nodes:["(del framework ", {tipox:'logo_tipox'}, ")."]}]}
+            ]},
+            {tipox:'div', nodes:[
+                'para ver la configuración y características de cada dispositivo ', 
+                {tipox:'button', nodes:['refrescar'], eventos:{click:mostrar}}
+            ]},
+            {tipox:'div', id:'muestra'}
+        ],
+        reemplazar:true
+    });
+    mostrar();
+});
