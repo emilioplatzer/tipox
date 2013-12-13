607ead8419678420b56d20cb4059a5c8c99a8928
emilioplatzer@gmail.com <emilioplatzer@gmail.com@25d175b5-64de-e66e-94b9-818158d59e08>
2013-12-13 03:20:20 +0000

    Ya hay una pantalla de adivinar y una de parado.

--- fuentes/alserver/gina/super.js
+++ fuentes/alserver/gina/super.js
@@ -7,13 +7,18 @@
                 {tipox:'div', style:{textAlign:'center'}, nodes:[
                     {tipox:'img', src:'gina.PNG', style:{'float':'left', marginRight:'10px'}},
                     {tipox:'img', src:'imagenes/torta.jpg', style:{'float':'right', marginRight:'10px'}},
-                    {tipox:'h1', nodes:'Bienvenidos al cumpleaños de Gina'},
-                    {tipox:'button', nodes:[{tipox:'b', nodes:'EMPEZAR AHORA'}, {tipox:'img', src:'imagenes/reloj.png'}]},
+                    {tipox:'h1', id:'titulo', nodes:'Bienvenidos al cumpleaños de Gina'},
+                    {tipox:'button', nodes:[ 
+                        {tipox:'img', id:'imagen_boton', src:'imagenes/reloj.png'}, 
+                        {tipox:'b', id:'texto_boton', nodes:'EMPEZAR AHORA'}
+                    ]},
                 ]},
                 {tipox:'div', nodes:[
-                    {tipox:'span', nodes:'presentes: '},
-                    {tipox:'span', id:'presentes', nodes:'todavía nadie'}
-                ]}
+                    {tipox:'br'},
+                    {tipox:'span', id:'label_jugadores', nodes:'presentes: '},
+                    {tipox:'span', id:'jugadores', nodes:'calculando...'}
+                ]},
+                {tipox:'div', id:'juego'}
             ]
         },
         reemplazar:true
@@ -25,13 +30,40 @@
                 datos:{hacer:'quienes'},
                 codificador:estoMismo,
                 cuandoOk:function(mensaje){
-                    presentes.innerText=mensaje.map(function(r){ return r.jugador;}).join(', ');
-                    presentes.className='ok';
+                    jugadores.innerText=mensaje.jugadores.map(function(r){ return r.jugador;}).join(', ')||'nadie aún';
+                    if(mensaje.empezado=='si'){
+                        label_jugadores.innerText='jugaron: ';
+                        titulo.innerText='Jugando en el cumple de Gina';
+                        if(mensaje.datos.estado==1){
+                            texto_boton.innerText='PARAR';
+                            imagen_boton.src='imagenes/reloj2.png';
+                        }else{
+                            texto_boton.innerText='SEGUIR';
+                            imagen_boton.src='imagenes/parar.png';
+                        }
+                        colocador.colocar({
+                            destino:juego,
+                            contenido:[
+                                {tipox:'img', src:'imagenes/'+mensaje.datos.imagen, className:'ilustracion_principal'}, 
+                                {tipox:'div', className:'pregunta', nodes:[
+                                    {tipox:'span', className:'numero_pregunta', nodes:mensaje.datos.juego+': '},
+                                    mensaje.datos.descripcion
+                                ]},
+                                {tipox:'div', nodes:mensaje.opciones.map(function(r){
+                                    return {tipox:'div', className:'opcion', nodes:[
+                                        {tipox:'span', className:'numero_opcion', nodes:r.opcion+': '}, r.texto
+                                    ]}
+                                })}
+                            ],
+                            reemplazar:true
+                        });
+                    }else{
+                    }
                     mirar();
                 },
                 cuandoFalla:function(mensaje,lugar){
-                    presentes.innerText=mensaje+' ('+lugar+')';
-                    presentes.className='falla';
+                    jugadores.innerText=mensaje+' ('+lugar+')';
+                    jugadores.className='falla';
                     mirar();
                 }
             });
