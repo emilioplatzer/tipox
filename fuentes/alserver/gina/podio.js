c1fbddb121c836091d1e4dbcdf616026b382d7f3
emilioplatzer@gmail.com <emilioplatzer@gmail.com@25d175b5-64de-e66e-94b9-818158d59e08>
2013-12-13 21:12:59 +0000

    Ya hicimos andar el podio.

--- fuentes/alserver/gina/podio.js
+++ fuentes/alserver/gina/podio.js
@@ -6,9 +6,9 @@
             {tipox:'div', className:'ala_izquierda', nodes:[
                 {tipox:'table', className:'tabla_podio', nodes:[
                     {tipox:'tr', nodes:[
-                        {tipox:'td', className:'podio_nombres', nodes:'Miranda', id:'quienes2', rowSpan:2},
-                        {tipox:'td', className:'podio_nombres', nodes:'Gina', id:'quienes1'},
-                        {tipox:'td', className:'podio_nombres', nodes:'Martina Joaquina Babina', id:'quienes3', rowSpan:3}
+                        {tipox:'td', className:'podio_nombres', nodes:'', id:'quienes2', rowSpan:2},
+                        {tipox:'td', className:'podio_nombres', nodes:'', id:'quienes1'},
+                        {tipox:'td', className:'podio_nombres', nodes:'', id:'quienes3', rowSpan:3}
                     ]},
                     {tipox:'tr', nodes:[
                         {tipox:'td', className:'podio_numeros', nodes:"1"},
@@ -42,7 +42,12 @@ function mirar_estado_juego(en_cuanto){
                 colocador.colocar({
                     destino:ganadores,
                     reemplazar:true,
-                    contenido:{tipox:'table', nodes:
+                    contenido:{tipox:'table', className:'ganadores', nodes:
+                        [{tipox:'tr', nodes:[
+                            {tipox:'th', nodes:'pos'},
+                            {tipox:'th', nodes:'jugador'},
+                            {tipox:'th', nodes:'puntos'}
+                        ]}].concat(
                         mensaje.jugadores.map(function(r){ 
                             // return {tipox:'span', nodes:r.jugador, className:mensaje.datos.estado<>2 || r.jugada<>mensaje.datos.correcta?'':'acerto'};
                             return {tipox:'tr', nodes:[
@@ -50,15 +55,29 @@ function mirar_estado_juego(en_cuanto){
                                 {tipox:'td', nodes:r.jugador},
                                 {tipox:'td', nodes:r.aciertos}
                             ]};
-                        })
+                        }))
                     }
                 });
-                mirar_estado_juego();
+                var posicion=0;
+                var puntos=0;
+                for(var i=0; i<mensaje.jugadores.length; i++){
+                    var jugador=mensaje.jugadores[i];
+                    if(jugador.aciertos!=puntos){
+                        puntos=jugador.aciertos;
+                        posicion++;
+                        if(posicion>3){
+                            break;
+                        }
+                    }
+                    var donde_poner_el_nombre=document.getElementById('quienes'+posicion);
+                    donde_poner_el_nombre.innerText+=' '+jugador.jugador;
+                }
+                // mirar_estado_juego();
             },
             cuandoFalla:function(mensaje,lugar){
                 ganadores.innerText=mensaje+' ('+lugar+')';
                 ganadores.className='falla';
-                mirar_estado_juego();
+                // mirar_estado_juego();
             }
         });
     },en_cuanto||1000);
