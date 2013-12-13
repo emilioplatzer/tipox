ab74e8063c71cfcc9c1d60dda8c34c63133695b4
emilioplatzer@gmail.com <emilioplatzer@gmail.com@25d175b5-64de-e66e-94b9-818158d59e08>
2013-12-13 20:53:17 +0000

    Ya logramos traer los datos del podio.

--- trunk/fuentes/alserver/gina/podio.js
+++ trunk/fuentes/alserver/gina/podio.js
@@ -6,8 +6,8 @@
             {tipox:'div', className:'ala_izquierda', nodes:[
                 {tipox:'table', className:'tabla_podio', nodes:[
                     {tipox:'tr', nodes:[
-                        {tipox:'td', className:'podio_nombres', nodes:'Gina', id:'quienes2', rowSpan:2},
-                        {tipox:'td', className:'podio_nombres', nodes:'Miranda', id:'quienes1'},
+                        {tipox:'td', className:'podio_nombres', nodes:'Miranda', id:'quienes2', rowSpan:2},
+                        {tipox:'td', className:'podio_nombres', nodes:'Gina', id:'quienes1'},
                         {tipox:'td', className:'podio_nombres', nodes:'Martina Joaquina Babina', id:'quienes3', rowSpan:3}
                     ]},
                     {tipox:'tr', nodes:[
@@ -23,92 +23,44 @@
                     ]},
                 ]}
             ]},
-            {tipox:'div', className:'ala_derecha', nodes:[
+            {tipox:'div', className:'ala_derecha', id:'ganadores', nodes:[
             ]}
         ]},
         reemplazar:true
     });
-    // mirar_estado_juego(500);
+    mirar_estado_juego(500);
 });
 
 function mirar_estado_juego(en_cuanto){
     setTimeout(function(){
         enviarPaquete({
             destino:'servir.php',
-            datos:{hacer:'quienes'},
+            datos:{hacer:'podio'},
             codificador:estoMismo,
             cuandoOk:function(mensaje){
                 var colocador=new Colocador();
                 colocador.colocar({
-                    destino:jugadores,
+                    destino:ganadores,
                     reemplazar:true,
-                    contenido:mensaje.jugadores.map(function(r){ 
-                        // return {tipox:'span', nodes:r.jugador, className:mensaje.datos.estado<>2 || r.jugada<>mensaje.datos.correcta?'':'acerto'};
-                        return {
-                            tipox:'span', 
-                            nodes:r.jugador, 
-                            className:'jugador'+(mensaje.datos.estado!=2 || r.jugada!=mensaje.datos.correcta?'':' acerto'),
-                            title:(mensaje.datos.estado==2?r.jugada:'')
-                        };
-                    })
-                });
-                if(mensaje.empezado=='si'){
-                    label_jugadores.innerText='jugaron: ';
-                    titulo.innerText='Jugando en el cumple de Gina';
-                    var jugando=mensaje.datos.estado==1;
-                    if(jugando){
-                        texto_boton.innerText='PARAR';
-                        imagen_boton.src='imagenes/reloj2.png';
-                    }else{
-                        texto_boton.innerText='SEGUIR';
-                        imagen_boton.src='imagenes/parar.png';
+                    contenido:{tipox:'table', nodes:
+                        mensaje.jugadores.map(function(r){ 
+                            // return {tipox:'span', nodes:r.jugador, className:mensaje.datos.estado<>2 || r.jugada<>mensaje.datos.correcta?'':'acerto'};
+                            return {tipox:'tr', nodes:[
+                                {tipox:'td', nodes:r.posicion},
+                                {tipox:'td', nodes:r.jugador},
+                                {tipox:'td', nodes:r.aciertos}
+                            ]};
+                        })
                     }
-                    colocador.colocar({
-                        destino:juego,
-                        contenido:[
-                            {tipox:'img', src:'imagenes/'+(jugando?mensaje.datos.imagen:mensaje.datos.imagenok), className:'ilustracion_principal'}, 
-                            {tipox:'div', className:'pregunta', nodes:[
-                                {tipox:'span', className:'numero_pregunta', nodes:mensaje.datos.juego+': '},
-                                mensaje.datos.descripcion
-                            ]},
-                            {tipox:'div', nodes:mensaje.opciones.map(function(r){
-                                return {
-                                    tipox:'div', 
-                                    className:'opcion'+(!jugando && r.opcion==mensaje.datos.correcta?' correcta':''), 
-                                    nodes:[
-                                        {tipox:'span', className:'numero_opcion', nodes:r.opcion+': '}, 
-                                        r.texto, 
-                                        {tipox:'span', className:'cuantos', nodes:(mensaje.datos.estado==2?r.cuantos:'')}
-                                    ]
-                                    , title:(mensaje.datos.estado==2?r.quienes:'')
-                                }
-                            })}
-                        ],
-                        reemplazar:true
-                    });
-                }else{
-                }
+                });
                 mirar_estado_juego();
             },
             cuandoFalla:function(mensaje,lugar){
-                jugadores.innerText=mensaje+' ('+lugar+')';
-                jugadores.className='falla';
+                ganadores.innerText=mensaje+' ('+lugar+')';
+                ganadores.className='falla';
                 mirar_estado_juego();
             }
         });
     },en_cuanto||1000);
 }
 
-function continuar_juego(evento){
-    enviarPaquete({
-        destino:'servir.php',
-        datos:{ hacer:'avanzar_juego'},
-        codificador:estoMismo,
-        cuandoOk:function(){
-            // mirar_estado_juego(500);
-        },
-        cuandoFalla:function(){
-            // mirar_estado_juego(500);
-        }
-    });
-}
